5af9d95052860f270aea0d22fcc378a53b2c8c4b
magalegaspi <magalegaspi@hotmail.com>
2025-06-18 22:57:10 -0300

    Base de datos funcionando

--- database/conexion.js
+++ database/conexion.js
@@ -1,18 +1,23 @@
 const sql = require('mssql');
-const configDB = require('./config').configDB
+const configDB = require('./config').configDB;
 
-exports.getSQLConnection = async () => {
-    console.log("invocando el método getSQLConnection")
+const getSQLConnection = async () => {
+    console.log("invocando el método getSQLConnection");
     try {
         const pool = await sql.connect(configDB);
 
-        //const resultado = await pool.request().query('select TOP 1 * from Lenguajes');
-        //console.log(resultado)
+        // Ejemplo de consulta (opcional):
+        // const resultado = await pool.request().query('SELECT TOP 1 * FROM Lenguajes');
+        // console.log(resultado.recordset);
 
-        return pool
+        return pool;
     } catch (error) {
-        console.log("Error en getConnection: " + error)
+        console.log("Error en getConnection: " + error);
     }
-}
+};
 
-//getSQLConnection();
+// Ejecutar si querés probar directamente
+getSQLConnection();
+
+// Exportar para usar en otros módulos
+exports.getSQLConnection = getSQLConnection;
